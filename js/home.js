1c2983ca4be1d6fa50e83c30e29bc2cfef7748df
Sujon Kumer <49690747+sujonkumer@users.noreply.github.com>
2023-07-19 00:09:27 +0600

    Initial commit

---
+++ js/home.js
@@ -0,0 +1,14 @@
+
+
+(function ($) {
+
+    'use strict';
+
+    //owlCarousel
+    $("#owl-testi").owlCarousel({
+        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        items: 2,
+        itemsDesktop: [1199, 2],
+        itemsDesktopSmall: [979, 2]
+    });
+})(jQuery)
